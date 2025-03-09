d810e001bd13ea18e16ca09f828e068211656290
Migiro-Johans <Migironyogora@gmail.com>
2025-03-09 01:44:41 -0800

    changeed author_name to author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,9 +3,9 @@ import os
 import django
 
 #Query all books by a specific author.
-def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author__name=author_name)
+def get_books_by_author(author):
+    author = Author.objects.get(name=author)
+    books = Book.objects.filter(author=author)
     return books
 #List all books in a library.
 def get_books_in_library(library_name):
