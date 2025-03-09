6e1146ee82147da761ae37eee6214abf7092526e
Migiro-Johans <Migironyogora@gmail.com>
2025-03-09 00:51:34 -0800

    first of many django-models

---
+++ django-models/django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,18 @@
+
+import os 
+import django
+
+#Query all books by a specific author.
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = author.books.all()
+    return books
+#List all books in a library.
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
+#Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
