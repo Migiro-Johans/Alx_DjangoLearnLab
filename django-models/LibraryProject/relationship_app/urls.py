b2930ef785a12c5e08e14166dc75b48c5943fa51
Migiro-Johans <Migironyogora@gmail.com>
2025-03-10 17:26:32 -0700

    addded the register and loginView, logoutview

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import list_books, LibraryDetaiView, register, user_login, user_logout
-
+from .views import list_books, LibraryDetaiView, user_login, user_logout
+rom django.contrib.auth.views import LoginView, LogoutView
+from .views import register
 urlpatterns = [
         path("books/",list_books, name ="list_books"),
         path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
