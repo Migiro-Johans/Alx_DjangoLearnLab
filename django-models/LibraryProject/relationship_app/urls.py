4b7b107900e39d5f93eaeda5bc54c78bf4c52d34
Migiro-Johans <Migironyogora@gmail.com>
2025-03-10 18:34:31 -0700

    new files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from .views import list_books, LibraryDetailView, user_login, user_logout
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import register
+from .views import admin_view, librarian_view, member_view
 urlpatterns = [
         path("books/",list_books, name ="list_books"),
         path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
         path('register/', register, name='register'),
         path('login/', LoginView.as_view(template_name='login.html'), name='login'),
         path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+        path('admin-dashboard/', admin_view, name='admin-dashboard'),
+        path('librarian-dashboard/', librarian_view, name='librarian-dashboard'),
+        path('member-dashboard/', member_view, name='member-dashboard'),
         ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,8 @@ from django.views.generic  import DetailView
 from .models import Library
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import UserProfile
 # Create your views here.
 #function-based view to list all books
 
@@ -39,4 +41,31 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('login')  # Redirect to login page after logout
+#user based role control
+# Role checking functions
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Admin View
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+# Librarian View
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+# Member View
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
