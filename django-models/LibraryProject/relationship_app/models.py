342b89e2990ea1ca094f4a4c24b917b4b4f66922
Migiro-Johans <Migironyogora@gmail.com>
2025-03-11 05:17:37 -0700

    models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,6 +12,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    published_date = models.DateField(null=True, blank=True)
     class Meta:
         permissions = [
             ("can_add_book", "Can add a new book"),
